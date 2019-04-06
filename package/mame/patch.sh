7c8a13d922d4c452599b9efcb625504d0d59f454
Subs <frog2wah@gmail.com>
2019-04-06 14:11:52 +0200

    part2

--- package/mame/patch.sh
+++ package/mame/patch.sh
@@ -1,4 +1,4 @@
 # Get mame version
 version=`grep 'pkgver=' /work/mame/trunk/PKGBUILD | cut -d "=" -f 2 | tr -d '.'`
-cp /work/package/mame/patch/0207_groovymame_017n.diff /work/mame/trunk
+cp /work/package/mame/patch/0208_groovymame_017n.diff /work/mame/trunk
 patch -p1 -d /work < /work/package/mame/patch/PKGBUILD.patch
