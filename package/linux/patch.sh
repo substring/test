926132930507e2b57dfc213a7998a1fbdb4f2068
Subs <frog2wah@gmail.com>
2019-04-11 01:19:48 +0200

    fix some sed crap

--- package/linux/patch.sh
+++ package/linux/patch.sh
@@ -1,6 +1,6 @@
 patch -p2 -d /work/linux < /work/package/linux/patch/PKGBUILD.patch
-sed -ir \
-    -e '/_srcname=archlinux-linux/a _15kpatchcommitid=393c0bca0ad8b908001027053f623d6bc96a9756' \
-    -e 's/pkgbase=linux[[:space:]]+/pkgbase=linux-15khz /' \
-    /work/linux/trunk/PKGBUILD
+sed -i \
+-e '/_srcname=archlinux-linux/a _15kpatchcommitid=393c0bca0ad8b908001027053f623d6bc96a9756' \
+-re 's/pkgbase=linux[[:space:]]+/pkgbase=linux-15khz /' \
+/work/linux/trunk/PKGBUILD
 exit $?
