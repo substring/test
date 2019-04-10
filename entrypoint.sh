84d49491c1fac1c4f530a4d52e93426381fb448f
Subs <frog2wah@gmail.com>
2019-04-10 13:26:35 +0200

    few cleans here and there, let some variables depend on CI

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-useradd -ms /bin/bash -d /work build
-echo 'build ALL=(root) NOPASSWD:ALL' > /etc/sudoers.d/user
-chmod 0440 /etc/sudoers.d/user
-su - build
--- release.sh
+++ release.sh
@@ -5,15 +5,13 @@ source settings
 _iso=groovyarcade_${GA_VERSION}.iso
 tag=${GA_VERSION}
 
-
-export GITHUB_USER=substring
-export GITHUB_REPO=test
-
 release_name="GroovyArcade $tag"
 ghr=`[[ -f ~/go/bin/github-release ]] && echo "~/go/bin/github-release" || echo "/usr/local/bin/github-release"`
 ghr_opts="--tag '$tag'"
 
+#
 # Create a release
+#
 $ghr release \
     --tag "$tag" \
     --name "groovyarcade $tag" \
@@ -32,13 +30,17 @@ cat packages_aur.lst packages_local.lst | grep -v "^#" | while read pkg ; do
   done
 done
 
+#
 # Upload the iso
+#
 $ghr upload \
     --tag "$tag" \
     --name "${_iso}.xz" \
     --file "work/output/${_iso}.xz"
 
+#
 # Make the release definitive
+#
 $ghr edit \
     --tag "$tag" \
     --name "groovyarcade $tag" \
