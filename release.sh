e729debd20e0263bcb989f42cd9150e1c05f9e46
Subs <frog2wah@gmail.com>
2019-05-01 14:48:57 +0200

    release.sh: takes arguments to set github release action

--- release.sh
+++ release.sh
@@ -3,26 +3,85 @@ set -e
 
 source settings
 
+cancel_and_exit() {
+  echo "Required cancel of release. Deleting the release" >&2
+  delete_release
+  exit 1
+}
+
+
+#
+# Create a release
+#
+function create_release () {
+	echo "Creating release $tag"
+$ghr release \
+    --tag "$tag" \
+    --name "$release_name" \
+    --description "automatic build" \
+    --pre-release
+}
+
+#
+# Upload repo + packages + iso
+#
+function upload_assets() {
+echo "Preparing the AUR repo"
+which repo-add && repo-add "${_output}"/groovyarcade.db.tar.gz "${_output}"/*.pkg.tar.xz
+
+while read -r file ; do
+  filename=$(basename "$file")
+  echo "Uploading $filename ..."
+  # Upload files
+  $ghr upload \
+    --tag "$tag" \
+    --name "$filename" \
+    --file "${_output}/$filename" || cancel_and_exit
+done < "${_output}"/built_packages
+
+# Upload the iso
+[[ ! -f ${_output}//${_iso}.xz ]] && cancel_and_exit
+echo "Uploading ${_iso}.xz..."
+$ghr upload \
+    --tag "$tag" \
+    --name "${_iso}.xz" \
+    --file "${_output}/${_iso}.xz" || cancel_and_exit
+}
+
+
+#
+# Make the release definitive
+#
+function publish_release () {
+	echo "Publihing release $tag"
+$ghr edit \
+    --tag "$tag" \
+    --name "GroovyArcade $tag" \
+    --description "automatic build"
+}
+
+#
+# Remove a release
+#
+function delete_release () {
+echo "Deleting release $tag..."
+$ghr delete \
+    --tag "$tag"
+}
+
 _iso=groovyarcade_${GA_VERSION}.iso
-_output=work
+if [[ -d /work ]] ; then
+  _output=/work/output
+elif [[ -d ./work ]] ; then
+  _output=./work/output
+else
+  echo "ERROR: no work dir found"
+  exit 1
+fi
 tag=${GA_VERSION}
 
 release_name="GroovyArcade $tag"
 ghr=$([[ -f ~/go/bin/github-release ]] && echo "$HOME/go/bin/github-release" || echo "/usr/local/bin/github-release")
-
-cancel_and_exit() {
-  echo "Required cancel of release. Deleting the release" >&2
-  $ghr delete --tag "$tag"
-  exit 1
-}
-
-#
-# Build repo
-#
-echo "Preparing the AUR repo"
-repo-add ${_output}/groovyarcade.db.tar.gz ${_output}/*.pkg.tar.xz
-
-echo "Getting ready for release $tag"
 
 # Make sure all env vars exist
 export GITHUB_TOKEN=${GITHUB_TOKEN:-$(cat ./GITHUB_TOKEN)}
@@ -34,41 +93,31 @@ if [[ -z $GITHUB_TOKEN ]] ; then
   exit 1
 fi
 
-#
-# Create a release
-#
-$ghr release \
-    --tag "$tag" \
-    --name "$release_name" \
-    --description "automatic build" \
-    --pre-release
+# Parse command line
+while getopts "cupd" option; do
+  case "${option}" in
+    c)
+      create_release
+      exit 0
+      ;;
+    u)
+      upload_assets
+      exit 0
+      ;;
+    p)
+      publish_release
+      exit 0
+      ;;
+    d)
+      delete_release
+      exit 0
+      ;;
+    *)
+      echo "ERROR: options can be -c -u -p or -d only" >&2
+      exit 1
+      ;;
+    esac
+done
 
-#
-# Upload packages
-#
-while read -r file ; do
-  filename=$(basename "$file")
-  echo "Uploading $filename ..."
-  # Upload files
-  $ghr upload \
-    --tag "$tag" \
-    --name "$filename" \
-    --file "$file"
-done < work/output/built_packages
-
-#
-# Upload the iso
-#
-[[ ! -f work/output/${_iso}.xz ]] && cancel_and_exit
-$ghr upload \
-    --tag "$tag" \
-    --name "${_iso}.xz" \
-    --file "work/output/${_iso}.xz"
-
-#
-# Make the release definitive
-#
-$ghr edit \
-    --tag "$tag" \
-    --name "groovyarcade $tag" \
-    --description "automatic build"
+echo "ERROR: no option provided" >&2
+exit 1
