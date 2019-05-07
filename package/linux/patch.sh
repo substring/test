e0591cd53bfb65f5f1492092d47a018af25776b9
Subs <frog2wah@gmail.com>
2019-05-07 10:04:40 +0200

    Bump to kernel 5.1

--- package/linux/patch.sh
+++ package/linux/patch.sh
@@ -1,6 +1,6 @@
 patch -p2 -d /work/linux < /work/package/linux/patch/PKGBUILD.patch
 sed -i \
--e '/_srcname=archlinux-linux/a _15kpatchcommitid=393c0bca0ad8b908001027053f623d6bc96a9756' \
+-e '/_srcname=archlinux-linux/a _15kpatchcommitid=cb50d2cf199e333b83260f1be5608e3ab19c4f29' \
 -re 's/pkgbase=linux[[:space:]]+/pkgbase=linux-15khz /' \
 /work/linux/trunk/PKGBUILD
 exit $?
